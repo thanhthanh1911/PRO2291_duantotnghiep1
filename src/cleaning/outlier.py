1834be6e1e47a422ca87493f0671c024f1e5969c
baolesg <baolesg@gmail.com>
2026-06-04 21:03:22 +0700

    oulier lam sach

--- src/cleaning/outlier.py
+++ src/cleaning/outlier.py
@@ -6,7 +6,7 @@ df = pd.read_csv("data/raw/sales_06_FY2020-21.csv")
 # Chọn các cột số
 numeric_cols = df.select_dtypes(include=['int64', 'float64']).columns
 
-print("=== OUTLIER REPORT ===\n")
+print("OUTLIER REPORT\n")
 
 for col in numeric_cols:
     Q1 = df[col].quantile(0.25)
@@ -26,3 +26,26 @@ for col in numeric_cols:
     print(f"Upper Bound: {upper_bound:.2f}")
     print(f"Number of Outliers: {len(outliers)}")
     print(f"Outliers:\n{outliers[[col]]}\n")
+
+
+# làm sạch dữ liệu bằng cách loại bỏ các outliers
+import pandas as pd
+
+df = pd.read_csv("data/raw/sales_06_FY2020-21.csv")
+
+clean_df = df.copy()
+
+for col in clean_df.select_dtypes(include=['int64', 'float64']).columns:
+    Q1 = clean_df[col].quantile(0.25)
+    Q3 = clean_df[col].quantile(0.75)
+    IQR = Q3 - Q1
+
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+
+    clean_df = clean_df[
+        (clean_df[col] >= lower_bound) &
+        (clean_df[col] <= upper_bound)
+    ]
+
+print("Số dòng sau khi loại bỏ outlier:", len(clean_df))
