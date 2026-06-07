0445af434ae1721f21610faa2300f551e50b094d
Fanta2k5 <fanta2k5@example.com>
2026-06-07 11:10:01 +0700

    hoàng thành làm sạch

--- src/cleaning/datatype.py
+++ src/cleaning/datatype.py
@@ -2,16 +2,11 @@ import pandas as pd
 
 
 def convert_datatypes(df):
-    """
-    Chuyển đổi kiểu dữ liệu cho DataFrame.
-    """
-
     df = df.copy()
 
     print("\n" + "=" * 50)
-    print("DATA TYPE CONVERSION")
+    print("CHUYỂN ĐỔI KIỂU DỮ LIỆU")
     print("=" * 50)
-
 
     date_cols = [
         "order_date",
@@ -19,51 +14,40 @@ def convert_datatypes(df):
     ]
 
     for col in date_cols:
-
         if col in df.columns:
-
             df[col] = pd.to_datetime(
-                df[col],
-                dayfirst=True,
-                errors="coerce"
+                df[col].astype(str).str.strip(),
+                errors="coerce",
+                format="mixed"
             )
 
-            print(f"[✔] Converted {col} → datetime")
-
-    # ------------------
-    # Chuyển cột số
-    # ------------------
+            print(f"Đã chuyển đổi {col} → datetime")
+            print(f"Null sau convert {col}: {df[col].isnull().sum():,}")
 
     numeric_cols = [
-
         "age",
-
         "qty_ordered",
         "price",
         "value",
         "discount_amount",
         "discount_percent",
         "total",
-
         "cust_id",
         "item_id",
         "year",
         "ref_num",
         "zip"
-
     ]
 
     for col in numeric_cols:
-
         if col in df.columns:
-
             df[col] = pd.to_numeric(
                 df[col],
                 errors="coerce"
             )
 
-            print(f"[✔] Converted {col} → numeric")
+            print(f"Đã chuyển đổi {col} → numeric")
 
-    print("[✔] Data type conversion completed.")
+    print("Chuyển đổi kiểu dữ liệu đã hoàn tất.")
 
     return df
